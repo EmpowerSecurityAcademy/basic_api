b1e47337522205c4cbba7404d524677ffeaa1f46
sheltowt <sheltowt@gmail.com>
2016-06-28 09:55:20 -0400

    adding updates

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST', 'PUT'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST'])
 def do_tasks():
 	if request.method == 'GET':
 		return jsonify({'tasks': tasks})
@@ -29,29 +29,29 @@ def do_tasks():
 		tasks.append(content)
 		return jsonify({'status_code': 201})
 
-	#return proper status_code is object not found in tasks
-	if request.method == 'PUT':
-		content = request.get_json(silent=True)
-		for x in tasks:
-			if x['id'] == content['id']:
-				x['title'] = content['title']
-				x['description'] = content['description']
-				x['done'] = content['done']
-		return jsonify({'status_code': 200})
-
 	return jsonify({'status_code': '400'})
 
 
 
 # RESTFUL operations related to a specific task
 
-@app.route('/todo/api/v1.0/tasks/<task_id>', methods=['GET', 'DELETE'])
+@app.route('/todo/api/v1.0/tasks/<task_id>', methods=['GET', 'DELETE', 'PUT'])
 def do_task(task_id):
 	task_id = int(task_id)
 	if request.method == 'GET':
 		for task in tasks:
 			if task['id'] == task_id:
 				return jsonify(task)
+
+	#return proper status_code is object not found in tasks
+	if request.method == 'PUT':
+		content = request.get_json(silent=True)
+		for x in tasks:
+			if x['id'] == task_id:
+				x['title'] = content['title']
+				x['description'] = content['description']
+				x['done'] = content['done']
+		return jsonify({'status_code': 200})
 
 	if request.method == 'DELETE':
 		for task in tasks:
